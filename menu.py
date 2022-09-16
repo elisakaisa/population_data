ba64ebd0ea94ed0a3b7c8c05ddfa679025a5303d
elisakaisa <elisa.k.perini@gmail.com>
2022-09-16 13:18:17 +0200

    graph for question 2c done

--- menu.py
+++ menu.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import sqlite3
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
 from sys import exit
 
 # for Python 2.x users
@@ -13,9 +17,9 @@ class Program:
         self.cur = self.conn.cursor() # create a database query cursor
 
         # specify the command line menu here
-        self.actions = [self.city_query, self.population_query, self.population_plot, self.population_avg_plot, self.exit]
+        self.actions = [self.city_query, self.population_query, self.population_plot, self.population_avg_plot, self.population_plot_predict, self.exit]
         # menu text for each of the actions above
-        self.menu = ["City query", "Population Query", "Plot Population", "Plot average population", "Exit"]
+        self.menu = ["City query", "Population Query", "Plot Population", "Plot average population", "Predict city population", "Exit"]
         self.cur = self.conn.cursor()
     def print_menu(self):
         """Prints a menu of all functions this program offers.  Returns the numerical correspondant of the choice made."""
@@ -95,7 +99,7 @@ class Program:
     #--------------------------- 2b ---------------------------
     def population_avg_plot(self):
         try:
-            query ="SELECT year,avg(population) FROM citypops GROUP BY year"
+            query ="SELECT year, avg(population) FROM citypops GROUP BY year"
             print("Will execute: ", query)
             self.cur.execute(query)
             result = self.cur.fetchall()
@@ -114,8 +118,49 @@ class Program:
                 xs.append(float(r[0]))
                 ys.append(float(r[1]))
                 
-        plt.scatter(xs, ys, s = 2)
+        plt.scatter(xs, ys, s = 10)
         plt.title('City population average')
+        plt.show()
+    
+    def population_plot_predict(self):
+        city = input("city: ")
+        print("city: %s" % (city))
+        try:
+            query ='SELECT year, population FROM citypops WHERE city == "%s"' % (city)
+            print("Will execute: ", query)
+            self.cur.execute(query)
+            result = self.cur.fetchall()
+        except sqlite3.Error as e:
+            print( "Error message:", e.args[0])
+            self.conn.rollback()
+            exit()
+
+        xs= []
+        ys= []
+        for r in result:
+            # you access ith component of row r with r[i], indexing starts with 0
+            # check for null values represented as "None" in python before conversion and drop
+            # row whenever NULL occurs
+            if (r[0]!=None and r[0]!=None):
+                xs.append(float(r[0]))
+                ys.append(float(r[1]))
+
+        print(xs)
+        #linear regression
+        regr = LinearRegression().fit(np.array(xs).reshape([-1,1]), np.array(ys).reshape([-1,1]))
+        score = regr.score(np.array(xs).reshape([-1,1]), np.array(ys).reshape([-1,1]))
+        print("The score of the linear regression is: %s" % (score))
+        a = regr.coef_[0][0]
+        b = regr.intercept_[0]
+
+        x_regression = np.array(xs).reshape([-1,1])
+        y_regression = a*x_regression + b
+        label = "%s * x + %s" % (a, b)
+
+        plt.scatter(xs, ys, s = 10)
+        plt.plot(xs, y_regression, '-r', label=label)
+        plt.legend(loc='upper left')
+        plt.title('City population of %s' % (city))
         plt.show()
 
     def exit(self):    
@@ -140,4 +185,3 @@ class Program:
 if __name__ == "__main__":
     db = Program()
     db.run()
-    
