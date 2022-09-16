e5a96dcc61b5c5e64853c909d21db18d00d97b70
elisakaisa <elisa.k.perini@gmail.com>
2022-09-16 15:15:49 +0200

    fixed issues, code ready for looping

--- figure.py
+++ figure.py
@@ -4,6 +4,8 @@ import sqlite3
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 
 connection1 = sqlite3.connect('mondial.db') # establish database connection
@@ -37,14 +39,23 @@ def drop():
         pass
 
 def init():
+    """ initialises the table """
     try:
         # Create table sales and add initial tuples
-        # new relation “linearprediction” with attributes “name, country, a, b, score” 
         query = "CREATE TABLE %s(name text, country text, a decimal, b decimal, score decimal);" % (table_name)
         cursor1.execute(query)
 
-        # TODO: loop through this with all cities
-        query = """INSERT INTO %s VALUES('SL', 'test', 2.45555, 4.3222222, 3.2)""" % (table_name);
+        # this commits all executed queries forming a transaction up to this point
+        connection1.commit()
+    except sqlite3.Error as e:
+        print("Error message:", e.args[0])
+        connection1.rollback()
+
+def add_row(city, country, a, b, score):
+    """ adds a row to the table initilaized in init() """
+    try:
+        #TODO: add check for nulls
+        query = """INSERT INTO %s VALUES('%s', '%s', %f, %f, %f)""" % (table_name, city, country, a, b, score);
         print("Query", query)
         cursor1.execute(query)
 
@@ -53,6 +64,39 @@ def init():
     except sqlite3.Error as e:
         print("Error message:", e.args[0])
         connection1.rollback()
+
+def population_predict():
+    """ calculates the linear regression of the given city """
+    city = input("city: ")
+    print("city: %s" % (city))
+    country = input("country: ")
+    try:
+        query ='SELECT year, population FROM citypops WHERE city == "%s" AND country == "%s"' % (city, country)
+        print("Will execute: ", query)
+        cursor1.execute(query)
+        result = cursor1.fetchall()
+    except sqlite3.Error as e:
+        print( "Error message:", e.args[0])
+        connection1.rollback()
+        exit()
+
+    xs= []
+    ys= []
+    for r in result:
+        # you access ith component of row r with r[i], indexing starts with 0
+        # check for null values represented as "None" in python before conversion and drop
+        # row whenever NULL occurs
+        if (r[0]!=None and r[0]!=None):
+            xs.append(float(r[0]))
+            ys.append(float(r[1]))
+
+    #linear regression
+    regr = LinearRegression().fit(np.array(xs).reshape([-1,1]), np.array(ys).reshape([-1,1]))
+    regr_score = regr.score(np.array(xs).reshape([-1,1]), np.array(ys).reshape([-1,1]))
+    print("The score of the linear regression is: %f" % (regr_score))
+    a = regr.coef_[0][0]
+    b = regr.intercept_[0]
+    return [city, country, a, b, regr_score]
 
 def query():
     # Here we test some concurrency issues.
@@ -99,10 +143,12 @@ def close():
 # when calling python filename.py the following functions will be executed:
 drop()
 init()
+
+# TODO: loop through this with all cities
+[name1, country1, a1, b1, score1] = population_predict()
+add_row(name1, country1, a1, b1, score1)
+
 [name, country, a, b, score] = query()
-#plt.scatter(xs, ys)
-#plt.savefig("figure.png") # save figure as image in local directory
-#plt.show()  # display figure if you run this code locally, otherwise comment out
 close()
 
 
